95a15c2a1d2dcd77f8a1884cd90d81d63c679d1a
amitgupta951 <amitgupta9573@gmail.com>
2023-09-30 10:08:23 +0530

    formatting done

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,27 @@
 // Node server which will handle socket io connections
-const io = require('socket.io')(8000)
+const io = require("socket.io")(8000);
 
-const users ={};
+const users = {};
 
-io.on('connection',socket=>{
-    // If any new user joins, let other users connected to the server know.
-    socket.on('new-user-joined', name =>{
-        console.log("New user", name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name)
+io.on("connection", (socket) => {
+  // If any new user joins, let other users connected to the server know.
+  socket.on("new-user-joined", (name) => {
+    console.log("New user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  // If someone sends a message, broadcast it to other people
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
     });
+  });
 
-    // If someone sends a message, broadcast it to other people
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-    });
-
-    // If someone leaves the chat, let other know
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
-})
+  // If someone leaves the chat, let other know
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
